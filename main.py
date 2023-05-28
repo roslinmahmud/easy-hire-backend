c3fa374024c4253d7edbf471c95a0ffa8fc854ad
Roslin Mahmud Joy <roslinmahmudjoy@gmail.com>
2023-05-28 22:43:24 +0600

    feat: upload resume API added for resumes folder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from pyresparser import ResumeParser
-from fastapi import FastAPI
+import shutil
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
@@ -7,3 +7,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Welcome to Easy Hire"}
+
+
+@app.post("/uploadresume/")
+async def upload_resume(resume: UploadFile):
+    file_location = f"resumes/{resume.filename}"
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(resume.file, buffer)
+    return {"filename": resume.filename}
