5166ca5dcfec235573797b170940818ff4a5d2ae
Roslin Mahmud Joy <Roslin.Joy@brainstation-23.com>
2023-06-07 18:16:06 +0600

    initialize db setup

---
+++ models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from database import Base
+from sqlalchemy.orm import relationship
+
+
+class Job(Base):
+    __tablename__ = "jobs"
+    id = Column(String, unique=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    responsibilities = Column(String)
+    requirements = Column(String)
+
+    resumes = relationship("Resume", back_populates="job")
+
+class Resume(Base):
+    __tablename__ = "resumes"
+    id = Column(String, unique=True, index=True)
+    college_name = Column(String)
+    email = Column(String)
+    phone = Column(String)
+    skills = Column(String)
+    total_experience = Column(String)
+    degree = Column(String)
+
+    job = relationship("Job", back_populates="resumes")
+
+
+
