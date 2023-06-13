2815f9851c1c8032fb23b18b6cc3ca68d007a5fc
Roslin Mahmud Joy <roslinmahmudjoy@gmail.com>
2023-06-13 22:06:28 +0600

    Update requirements.txt

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Format: "postgresql://username:password@localhost/easy-hire"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/easy-hire"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:unlock@localhost/easy-hire"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
