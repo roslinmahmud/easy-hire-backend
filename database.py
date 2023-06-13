a9b4f4dd4a52e2083b281a134b144077f180fb19
Farhan Mohd. Fokrul Alam <metalhead901@live.com>
2023-06-13 23:47:39 +0600

    update connstring

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Format: "postgresql://username:password@localhost/easy-hire"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:unlock@localhost/easy-hire"
+SQLALCHEMY_DATABASE_URL = f'postgresql://anonymous.jak:ZGKQbc0LqUo8@ep-broad-field-924328.us-east-2.aws.neon.tech/easy-hire'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
