f2b0d7b2165c8a1c14d789e040a1f80a6699b925
Roslin Mahmud Joy <Roslin.Joy@brainstation-23.com>
2023-06-16 12:13:02 +0600

    Add dotenv file for configurations.

--- database.py
+++ database.py
@@ -1,9 +1,15 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
+
+
 # Format: "postgresql://username:password@localhost/easy-hire"
-SQLALCHEMY_DATABASE_URL = f'postgresql://anonymous.jak:ZGKQbc0LqUo8@ep-broad-field-924328.us-east-2.aws.neon.tech/easy-hire'
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONNECTION_STRING")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
